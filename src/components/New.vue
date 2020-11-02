3fd8c4a740a97613007fd4691dd8dfa7e2886269
dyh <xinzon32177@163.com>
2020-11-03 00:08:52 +0800

    init

---
+++ src/components/New.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="new">
+    <div class="p1">{{ p1 }}</div>
+    <div class="p2">{{ p2.a + p2.b }}</div>
+  </div>
+</template>
+
+<script lang="ts" setup="props,ctx">
+declare const props: {
+  p1: string;
+  p2: { a: number; b: number };
+};
+</script>
+
+<style scoped></style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,21 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" />
+    <img alt="Vue logo" src="../assets/logo.png"/>
+    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+    <New p1="hello world" :p2="{a:1,b:2}"></New>
   </div>
 </template>
 
 <script lang="ts">
-import { defineComponent } from "vue";
+import {defineComponent} from "vue";
 import HelloWorld from "@/components/HelloWorld.vue"; // @ is an alias to /src
+import New from "@/components/New.vue";
 
 export default defineComponent({
   name: "Home",
   components: {
-    HelloWorld
-  }
+    HelloWorld,
+    New,
+  },
 });
 </script>
