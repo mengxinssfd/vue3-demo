5d0ba6f1a9ecfb3761ab7737c80c75f8ab19224f
dengyonghua <xinzon32177@163.com>
2020-12-04 18:00:29 +0800

    loading

--- src/components-ts/Loading.ts
+++ src/components-ts/Loading.ts
@@ -1,30 +1,37 @@
 // loading/index.js
-import { createApp, ComponentPublicInstance, reactive } from "vue";
+import { createApp, ComponentPublicInstance, reactive, ref } from "vue";
 import LoadingComponent from "@/components/Loading.vue";
 
 let loading: { close: () => void } | undefined = undefined;
 let closeTimer: number;
 // fixme 使用reactive函数生成的object传递给组件不会响应
-const re = reactive({ value: true, msg: "" });
+// const re = reactive({value: true, msg: ""});
+const visible = ref(false);
 
-export default function(msg?: string) {
-  re.value = true;
-  re.msg = msg ?? re.msg;
+export default function (msg?: string) {
   if (loading) {
+    visible.value = true;
     clearTimeout(closeTimer);
     return loading;
   }
+  visible.value = false;
   const parent = document.body;
   const el = document.createElement("div");
 
-  const app = createApp(LoadingComponent, re);
+  const app = createApp(LoadingComponent, {value: visible, msg});
   const ins = app.mount(el) as ComponentPublicInstance<typeof LoadingComponent>;
 
   parent.appendChild(ins.$el);
 
+  const a = 100_000_000
+
+  ins.$nextTick(() => {
+    visible.value = true;
+  });
+
   return (loading = {
     close() {
-      re.value = false;
+      visible.value = false;
       closeTimer = setTimeout(() => {
         loading = undefined;
         app.unmount(el);
--- src/components/Loading.vue
+++ src/components/Loading.vue
@@ -2,14 +2,16 @@
   <transition name="custom-loading-fade">
     <!--loading蒙版-->
     <div v-show="visible" class="custom-loading-mask">
-      <!--loading中间的图标-->
-      <div class="custom-loading-spinner">
-        <svg viewBox="25 25 50 50" class="circular">
-          <circle cx="50" cy="50" r="20" fill="none" class="path"></circle>
-        </svg>
+      <div class="custom-loading-content">
+        <!--loading中间的图标-->
+        <div class="custom-loading-spinner">
+          <svg viewBox="25 25 50 50" class="circular">
+            <circle cx="50" cy="50" r="20" fill="none" class="path"></circle>
+          </svg>
+        </div>
+        <!--loading下面显示的文字-->
+        <p class="custom-loading-text">{{ text }}</p>
       </div>
-      <!--loading下面显示的文字-->
-      <p class="custom-loading-text">{{ text }}</p>
     </div>
   </transition>
 </template>
@@ -34,7 +36,7 @@ export default defineComponent({
         console.log("cccccccccccccc");
         visible.value = n;
       },
-      { immediate: true }
+      {immediate: true}
     );
 
     function setText(msg: string) {
@@ -70,16 +72,13 @@ export default defineComponent({
 .custom-loading-fade-leave-active {
   transition: opacity 0.3s linear;
 }
-
 .custom-loading-fade-enter-from,
 .custom-loading-fade-leave-to {
   opacity: 0;
 }
-
 body > .custom-loading-mask {
   position: fixed;
 }
-
 .custom-loading-mask {
   position: absolute;
   left: 0;
@@ -89,19 +88,30 @@ body > .custom-loading-mask {
   background: rgba(0, 0, 0, 0.5);
   z-index: 100000;
 }
-
-.custom-loading-spinner {
+.custom-loading-content {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
   position: absolute;
   left: 50%;
   top: 50%;
   transform: translate(-50%, -50%);
   color: white;
-
+  .custom-loading-text {
+    text-align: center;
+    &:after {
+      margin-left: 4px;
+      letter-spacing: 4px;
+      animation: 2s linear infinite running loading-end;
+      content: "...";
+    }
+  }
+}
+.custom-loading-spinner {
   .circular {
     height: 42px;
     width: 42px;
     animation: loading-rotate 2s linear infinite;
-
     .path {
       animation: loading-dash 1.5s ease-in-out infinite;
       stroke-dasharray: 90, 150;
@@ -113,7 +123,6 @@ body > .custom-loading-mask {
     }
   }
 }
-
 @keyframes loading-dash {
   0% {
     stroke-dasharray: 1, 200;
@@ -128,7 +137,6 @@ body > .custom-loading-mask {
     stroke-dashoffset: -120px;
   }
 }
-
 @keyframes fade-in {
   from {
     opacity: 0;
@@ -137,10 +145,26 @@ body > .custom-loading-mask {
     opacity: 1;
   }
 }
-
 @keyframes loading-rotate {
   100% {
     transform: rotate(1turn);
+  }
+}
+@keyframes loading-end {
+  0% {
+    content: "...";
+  }
+  25% {
+    content: "·..";
+  }
+  50% {
+    content: ".·.";
+  }
+  75% {
+    content: "..·";
+  }
+  100% {
+    content: "...";
   }
 }
 </style>
